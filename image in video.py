8bd247b76c4e285cba9941e01fd351337f00d92d
vinaylalwani <109392603+vinaylalwani@users.noreply.github.com>
2023-12-02 19:28:16 -0800

    abc
    abc

--- image in video.py
+++ image in video.py
@@ -9,24 +9,16 @@ vid = cv2.VideoCapture('video.mp4')
 while True:
     # Read the next frame
     ret, frame = vid.read()
-    
     if not ret:
         break
-
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect image in the grayscale frame
+    # Detect image
     image= image_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
     # Draw a rectangle around each detected face
     for (x, y, w, h) in imageimage:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
+      cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     cv2.imshow('Video', frame)
-
-    # Wait for a key press
     if cv2.waitKey(1) == ord('q'):
         break
 
